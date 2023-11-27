440c3b4945110d0688a620733acabcc96517f736
alamin <alaminakash9754@gmail.com>
2023-11-28 00:13:29 +0600

    I've stored user data in the backend and granted admin role to logged-in users

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i6c2rzu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,12 @@ async function run() {
 
 
         // users related api
+        app.get('/users', async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
@@ -42,7 +48,17 @@ async function run() {
             res.send(result);
         })
 
-
+        app.patch('/users/admin/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id)}
+            const updatedDoc = {
+              $set: {
+                role: 'admin'
+              }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+          })
 
 
 
