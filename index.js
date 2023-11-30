cf802d8fab852b4c7eed3f1574fa6c044a005196
alamin <alaminakash9754@gmail.com>
2023-11-30 21:31:49 +0600

    admin get all data from mongodb server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -28,12 +29,66 @@ async function run() {
         // await client.connect();
 
         const userCollection = client.db("inventifyHub").collection("users");
+        const shopCollection = client.db("inventifyHub").collection("shops");
+        const productCollection = client.db("inventifyHub").collection("products");
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+        // middlewares verify token 
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // verify admin after verifYToken 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
 
 
         // users related api
-        app.get('/users', async(req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            console.log(req.headers);
             const result = await userCollection.find().toArray();
             res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
         })
 
 
@@ -48,23 +103,99 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async(req, res) => {
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
-              $set: {
-                role: 'admin'
-              }
+                $set: {
+                    role: 'admin'
+                }
             }
             const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        // shop related api
+        app.post('/shops',verifyToken, async(req,res) => {
+            const shopItem = req.body;
+            const result = await shopCollection.insertOne(shopItem);
+            res.send(result);
+        })
+
+        app.get('/shops', async(req, res) => {
+            const result = await shopCollection.find().toArray();
+            res.send(result)
+        });
+
+        app.get('/shops',verifyToken, async(req, res) => {
+            const email = req.query.email;
+            console.log(email)
+            const query = {ownerEmail: email};
+            const result = await shopCollection.find(query).toArray();
+            res.send(result);
+            console.log(result)
+          })
+
+        // Product Related api
+
+        app.post('/products',verifyToken, async(req,res) => {
+            const productItem = req.body;
+            const result = await productCollection.insertOne(productItem);
+            res.send(result);
+        })
+
+        app.get('/products',verifyToken, async(req, res) => {
+            const email = req.query.email;
+            // console.log(email)
+            const query = {email: email};
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+            // console.log(result)
+          })
+
+
+          app.get('/products/:id',verifyToken, async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            console.log(query)
+            const result = await productCollection.findOne(query);
+            res.send(result);
+          })
+        
+          app.patch('/products/:id',verifyToken, async(req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc = {
+              $set: {
+                name: item.name,
+                quantity: item.quantity,
+                cost: item.cost,
+                details: item.details,
+                image: item.image,
+                location: item.location,
+                discount: item.discount,
+                profit: item.profit,
+                price: item.price
+
+              }
+            }
+            const result = await productCollection.updateOne(filter, updatedDoc);
+            res.send(result)
+          })
+
+          app.delete('/products/:id',verifyToken, async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id) }
+            // console.log(query);
+            const result = await productCollection.deleteOne(query);
             res.send(result);
           })
 
 
-
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
